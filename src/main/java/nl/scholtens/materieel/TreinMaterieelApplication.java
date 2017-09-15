97bc46023472f9a60fa0fe066103beb9cb572dfa
Bert Scholtens <bert.scholtens@gmail.com>
2017-09-15 21:28:40 +0200

    init project Treinmaterieel

---
+++ src/main/java/nl/scholtens/materieel/TreinMaterieelApplication.java
@@ -0,0 +1,12 @@
+package nl.scholtens.materieel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreinMaterieelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreinMaterieelApplication.class, args);
+	}
+}
