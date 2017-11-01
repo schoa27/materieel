209c331392f1964738a843e57cd201d821549a49
Bert Scholtens <bert.scholtens@gmail.com>
2017-11-01 20:34:01 +0100

    Javascript voor zoeken toegevoegd

---
+++ src/main/resources/static/js/inputToggle.js
@@ -0,0 +1,33 @@
+var owner = document.getElementById('owner'),
+    catalog = document.getElementById('catalog'),
+    dcc = document.getElementById('dcc'),
+    br = document.getElementById('br');
+
+
+function  enableToggle(current, other) {
+    other.disabled = current.value.replace(/\s+/, '').length > 0;
+}
+
+owner.onkeyup = function () {
+    enableToggle(this, catalog);
+    enableToggle(this, dcc);
+    enableToggle(this, br);
+}
+
+catalog.onkeyup = function () {
+    enableToggle(this, owner);
+    enableToggle(this, dcc);
+    enableToggle(this, br);
+}
+
+dcc.onkeyup = function () {
+    enableToggle(this, owner);
+    enableToggle(this, catalog);
+    enableToggle(this, br);
+}
+
+br.onkeyup = function () {
+        enableToggle(this, owner);
+        enableToggle(this, catalog);
+        enableToggle(this, dcc);
+    }
