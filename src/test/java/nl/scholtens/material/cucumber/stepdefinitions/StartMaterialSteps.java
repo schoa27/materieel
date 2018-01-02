21c094c13c895494d114607e7feb30bf588cd45d
Bert Scholtens <bert.scholtens@gmail.com>
2018-01-02 15:11:01 +0100

    StartMaterialSteps uit gebreid met een senario

--- src/test/java/nl/scholtens/material/cucumber/stepdefinitions/StartMaterialSteps.java
+++ src/test/java/nl/scholtens/material/cucumber/stepdefinitions/StartMaterialSteps.java
@@ -1,4 +1,4 @@
-package nl.scholtens.material.cucumber.stepfefinitions;
+package nl.scholtens.material.cucumber.stepdefinitions;
 
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -6,11 +6,14 @@ import cucumber.api.java.en.When;
 import nl.scholtens.material.cucumber.MaterialFeatureTest;
 import nl.scholtens.material.cucumber.driver.Driver;
 import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.Matchers.comparesEqualTo;
 import static org.hamcrest.Matchers.not;
 import static org.hamcrest.core.Is.is;
+import static org.hamcrest.text.IsEqualIgnoringCase.equalToIgnoringCase;
 
 
 public class StartMaterialSteps extends MaterialFeatureTest {
@@ -38,4 +41,31 @@ public class StartMaterialSteps extends MaterialFeatureTest {
         Assert.assertThat(driver.getCurrentUrl(), is("http://localhost:8085/setup"));
         driver.close();
     }
+
+
+    @Given("^De browser is gestart$")
+    public void deBrowserIsGestart() throws Throwable {
+     driver = Driver.getWebDriverChrome();
+     Assert.assertThat(driver, not(nullValue()));
+     driver.get("http://localhost:8085/material");
+    }
+
+    @Given("^De setup pagina is geopend$")
+    public void deSetupPaginaIsGeopend() throws Throwable {
+      Assert.assertThat(driver.getTitle(), comparesEqualTo("Material Overview"));
+    }
+
+    @When("^Selecteer de Nederlandse taal in Dropdown overzicht Change the language$")
+    public void selecteerDeNederlandseTaalInDropdownOverzichtChangeTheLanguage() throws Throwable {
+        driver.findElement(By.id("locales")).findElement(By.name("nl")).click();
+    }
+
+    @Then("^De Setup pagina wordt getoond in het nederlands getoond$")
+    public void deSetupPaginaWordtGetoondInHetNederlandsGetoond() throws Throwable {
+        Assert.assertThat(driver.findElement(By.name("nl")).getText(), equalToIgnoringCase("Nederlands"));
+        driver.close();
+    }
+
+
+
 }
--- src/test/resources/features/startmaterial.feature
+++ src/test/resources/features/startmaterial.feature
@@ -4,3 +4,9 @@ Feature: Starten van Materieel overzicht
     Given De browser wordt gestart
     When Het adres van Materieel Overzicht wordt in gevuld in dres balk
     Then Setup pagina van Materieel Overzicht wordt getoond
+
+  Scenario: De browser is gestart en staat in de setup pagina
+    Given De browser is gestart
+    Given De setup pagina is geopend
+    When Selecteer de Nederlandse taal in Dropdown overzicht Change the language
+    Then De Setup pagina wordt getoond in het nederlands getoond
